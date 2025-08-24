adbbf07441d175ee6b5960b21c22c1da61b018a2
Rayiumir <swordart2050@gmail.com>
2025-08-25 00:40:57 +0330

    Writing database code and connecting to Mongodb

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const {connect} = require("mongoose");
+const connectDB = require("./config/db");
 const app = express();
 
 // Middleware to handle CORS
@@ -12,6 +14,10 @@ app.use(
         allowedHeaders: ["Content-Type", "Authorization"],
     })
 );
+
+// Connect Database
+
+connectDB();
 
 // Middleware
 app.use(express.json());
