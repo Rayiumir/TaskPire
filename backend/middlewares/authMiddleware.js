7f2275342ba0f39c5eff5b0bc67e2c6cc453c950
Rayiumir <swordart2050@gmail.com>
2025-08-27 02:57:49 +0330

    Middleware to Protect Routes and Middleware for Admin Access

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const {message} = require("multer/lib/multer-error");
+
+// Middleware to Protect Routes
+const protect = async (req, res, next) => {
+    try {
+        let token = req.headers.authorization;
+        if(token && token.startsWith("Bearer")){
+            token = token.split("")[1];
+            const decoded = jwt.verify(token, process.env.SECRET);
+            req.user = await User.findById(decoded.id).select("-password");
+            next();
+        }else {
+            res.status(401).json({message: "Not authorized, no token"});
+        }
+    }catch(err){
+        res.status(401).json({message: "Token failed", error: message});
+    }
+};
+
+// Middleware for Admin Access
+
+const adminLogin = (req, res, next) => {
+    if (req.user && req.user.role === "admin") {
+        next();
+    } else {
+        res.status(403).json({message: "Access denied, Administrator"});
+    }
+};
+
+module.exports = {protect, adminLogin};
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const {loginUser, registerUser, getUserProfile, updateUserProfile} = require("../controllers/authController");
+const {protect} = require("../middlewares/authMiddleware");
 
 // Auth Routes
-
 router.post("/login", loginUser);
 router.post("/register", registerUser);
 router.get("/profile", protect, getUserProfile);
