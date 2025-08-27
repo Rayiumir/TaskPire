be3eaf5b61bd8f0ae7b0c1342ce86fb22f50bf37
Rayiumir <swordart2050@gmail.com>
2025-08-27 15:59:23 +0330

    Writing Code registerUser in authController

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,7 +4,43 @@ const generateToken = (userId) => {
 }
 
 const loginUser = async (req, res) => {};
-const registerUser = async (req, res) => {};
+const registerUser = async (req, res) => {
+    try {
+        const { name, email, password, profileImageURL, adminInviteToken } = req.body;
+        const userExists = await User.findOne({ email });
+        if (userExists) {
+            return res.status(400).json({message: 'User already exists'});
+        }
+        let role = "user";
+        if(adminInviteToken && adminInviteToken === process.env.ADMIN_INVITE_TOKEN) {
+            role = "admin";
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password, salt);
+
+        // Create new User
+        const user = await User.create({
+            name,
+            email,
+            password: hashPassword,
+            profileImageURL,
+            role,
+        });
+
+        // Return User Data with JWT
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            profileImageURL: user.profileImageURL,
+            token: generateToken(user._id),
+        });
+
+    }catch (error) {
+        res.status(500).json({message: "Server error", error: error.message});
+    }
+};
 const getUserProfile = async (req, res) => {};
 const updateUserProfile = async (req, res) => {};
 
