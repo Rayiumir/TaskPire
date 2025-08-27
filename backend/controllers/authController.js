ee928673059631b3249836f9042b2243fe5aad43
Rayiumir <swordart2050@gmail.com>
2025-08-27 20:21:25 +0330

    Writing Code getUserProfile in authController

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -70,7 +70,16 @@ const registerUser = async (req, res) => {
         res.status(500).json({message: "Server error", error: error.message});
     }
 };
-const getUserProfile = async (req, res) => {};
+const getUserProfile = async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select("-password");
+        if (!user) {
+            return res.status(404).json({message: "User not found"});
+        }
+    }catch (error) {
+        res.status(500).json({message: "Server error", error: error.message});
+    }
+};
 const updateUserProfile = async (req, res) => {};
 
 module.exports = {loginUser, registerUser, getUserProfile, updateUserProfile};
