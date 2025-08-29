46164d4a4455a807b6166bdc3a493cc23b34786f
Rayiumir <swordart2050@gmail.com>
2025-08-29 03:46:56 +0330

    Completion Code taskRoutes and taskController

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -7,15 +7,15 @@ const protect = async (req, res, next) => {
     try {
         let token = req.headers.authorization;
         if(token && token.startsWith("Bearer")){
-            token = token.split("")[1];
-            const decoded = jwt.verify(token, process.env.SECRET);
+            token = token.split(" ")[1];
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
             req.user = await User.findById(decoded.id).select("-password");
             next();
         }else {
             res.status(401).json({message: "Not authorized, no token"});
         }
     }catch(err){
-        res.status(401).json({message: "Token failed", error: message});
+        res.status(401).json({message: "Token failed", error: err.message});
     }
 };
 
---
+++ backend/routes/taskRoutes.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const {protect, adminAccess} = require("../middlewares/authMiddleware");
+const {
+    getDashboardData,
+    getUserData,
+    getTasks,
+    getTaskById,
+    createTask,
+    updateTask,
+    deleteTask,
+    updateTaskStatus,
+    updateTaskChecklist
+} = require("../controllers/taskController");
+const router = express.Router();
+
+// Task Management Routes
+
+router.get("/dashborad-data", protect, getDashboardData);
+router.get("/user-data", protect, getUserData);
+
+router.get("/", protect, getTasks);
+router.get("/:id", protect, getTaskById);
+
+router.post("/", protect, adminAccess, createTask);
+router.put("/:id", protect, adminAccess, updateTask);
+router.delete("/:id", protect, adminAccess, deleteTask);
+router.put("/:id/status", protect, updateTaskStatus);
+router.put("/:id/tode", protect, updateTaskChecklist);
+
+module.exports = router;
