9b51df8870c23509ee5a7f2708bc1987ca697e6b
Rayiumir <swordart2050@gmail.com>
2025-08-30 17:53:20 +0330

    Writing Code apiPaths

---
+++ frontend/TaskPire/src/utils/apiPaths.js
@@ -0,0 +1,39 @@
+export const API_URL = 'http://localhost:8000';
+
+// utils/apiPaths.js
+export const API_PATHS = {
+    AUTH: {
+        LOGIN: "/api/auth/login",
+        REGISTER: "/api/auth/register",
+        GET_PROFILE: "/api/auth/profile",
+    },
+
+    USERS: {
+        GET_ALL_USERS: "/api/users",
+        GET_USER_BY_ID: (userId) => `/api/users/${userId}`,
+        CREATE_USER: "/api/users",
+        UPDATE_USER: (userId) => `/api/users/${userId}`,
+        DELETE_USER: (userId) => `/api/users/${userId}`,
+    },
+
+    TASKS: {
+        GET_DASHBOARD_DATA: "/api/tasks/dashboard-data",
+        GET_USER_DATA: "/api/tasks/user-data",
+        GET_ALL_TASKS: "/api/tasks",
+        GET_TASK_BY_ID: `/api/tasks/${taskId}`,
+        CREATE_TASK: "/api/tasks",
+        UPDATE_TASK: (taskId) => `/api/tasks/${taskId}`,
+        DELETE_TASK: (taskId) => `/api/tasks/${taskId}`,
+        UPDATE_TASK_STATUS: (taskId) => `/api/tasks/${taskId}/status`,
+        UPDATE_TASK_CHECKLIST: (taskId) => `/api/tasks/${taskId}/tode`,
+    },
+
+    REPORTS: {
+        EXPORT_TASKS: "/api/reports/export/tasks",
+        EXPORT_USERS: "/api/reports/export/users",
+    },
+
+    IMAGE: {
+        UPLOAD_IMAGE: "/api/auth/uploadImage",
+    }
+};
