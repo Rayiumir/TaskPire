85deca940f4cde4f66b8665b3f68d5de953ab598
Rayiumir <swordart2050@gmail.com>
2025-09-04 18:38:49 +0330

    Create AdminIndex

--- frontend/TaskPire/src/pages/Admin/AdminIndex.jsx
+++ frontend/TaskPire/src/pages/Admin/AdminIndex.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, {useContext} from "react";
+import AdminLayout from "../../components/layouts/AdminLayout.jsx";
+import {useUserAuth} from "../../hooks/useUserAuth.jsx";
+import {UserContext} from "../../context/userContext.jsx";
+import {useNavigate} from "react-router-dom";
+import axiosInstance from "../../utils/axiosInstance.js";
+import {API_PATHS} from "../../utils/apiPaths.js";
 
 const AdminIndex = () => {
-    return (
-        <div>Dashboard</div>
-    )
+    useUserAuth();
+    const {user} = useContext(UserContext);
+    const navigate = useNavigate();
+
+    const getUserData = async () => {
+        try {
+            const response = await axiosInstance.get(API_PATHS.TASKS.GET_DASHBOARD_DATA);
+            if (response.data){
+                setUserData(response.data);
+            }
+        }catch (error){
+            console.error("Error fetching users:", error);
+        }
+    };
+
+    return <AdminLayout activeMenu="AdminIndex"></AdminLayout>
 }
 
 export default AdminIndex
